d397dd19a2aa7d41bd68e170b9b5bd878a31bca1
Lucas <leonczyklucas@gmail.com>
2025-07-17 15:09:13 -0300

    Creando componentes de Descripcion.tsx

--- src/Hooks/artistasData.ts
+++ src/Hooks/artistasData.ts
@@ -1,11 +1,9 @@
-// artistasData.ts
 export const artistas = [
   {
     id: "Porcupine Tree",
     nombre: "Porcupine Tree",
     descripcion: "Porcupine Tree es una banda inglesa formada en 1985...",
-    imagen: "../public/Porcupine.jpg",
-    // más info si querés
+    imagen: "../public/Porcupine.jpg",    
   },
   // otros artistas
 ];
---
+++ src/Hooks/cancionesLista.ts
@@ -0,0 +1,38 @@
+export const cancionesLista = [
+   {
+      titulo: "The Incident - Porcupine Tree",
+      audioUrl: "/Musica/porcupine_tree__the_incident.mp3",
+      arte: "https://m.media-amazon.com/images/I/41kBPpqJbiL._SX300_SY300_QL70_FMwebp_.jpg",
+      año: "2009",
+    },
+    {
+      titulo: "Sex Tape - Deftones",
+      audioUrl: "/Musica/Sextape.mp3",
+      arte: "https://images.squarespace-cdn.com/content/v1/5147d98fe4b0e61bb0ab60ec/1363845787383-JGAGB2GKWBGOEL08VMHZ/Deftones+DE+Cover+300+rgb.jpg?format=1500w",
+      año: "2000",
+    },
+    {
+      titulo: "Parabola - Tool",
+      audioUrl: "/Musica/Parabola.mp3",
+      arte: "https://lastfm.freetls.fastly.net/i/u/770x0/ec676167abeb99f85d6fee875d55251e.jpg",
+      año: "2001",
+    },
+    {
+      titulo: "Birds of feather - Billie Eilish",
+      audioUrl: "/Musica/BIRDS OF A FEATHER.mp3",
+      arte: "https://cdn-images.dzcdn.net/images/cover/5d284b31cb9ddeb1a0c79aede5a94e1c/500x500-000000-80-0-0.jpg",
+      año: "2024",
+    },
+    {
+      titulo: "Digital Bath - Deftones",
+      audioUrl: "/Musica/DigitalBath.mp3",
+      arte: "https://cdn-images.dzcdn.net/images/cover/c2942294fe29f749ca9ed2d1b25bb247/500x500-000000-80-0-0.jpg",
+      año: "2021",
+    },
+    {
+      titulo: "El último de todos - Anomia",
+      audioUrl: "/Musica/el_ultimo_de_todos.mp3",
+      arte: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnEpwOoCFlWPXaGrK3rScKMyLeVezt0HEVdUJ1NPZyfPsfkaJU1O6hfD3DgDpfilDqmPc&usqp=CAU",
+      año: "2022",
+    },
+];
