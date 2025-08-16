319a557383bf2469497acebd733c119780f569f7
Lucas Leonczyk <leonczyklucas@gmail.com>
2025-08-16 15:38:07 -0300

    seteando rutas para vercel

--- src/Hooks/cancionesData.ts
+++ src/Hooks/cancionesData.ts
@@ -3,7 +3,7 @@ export const canciones = [
     id: "1",
     nombre: "Porcupine Tree",
     descripcion: "Porcupine Tree es una banda inglesa formada en 1985...",
-    imagen: "../public/Porcupine.jpg",
+    imagen: "/Porcupine.jpg",
     // más info si querés
   },
   // otros artistas
